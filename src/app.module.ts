c2d3bf4c77a2a315b3663b86de417422d052692e
FarruxDEV <farruxbeknozimov@gmail.com>
2023-06-25 17:54:10 +0500

    add tables to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AdminsModule } from './admins/admins.module';
+import { AssistantsModule } from './assistants/assistants.module';
+import { DirectionsModule } from './directions/directions.module';
+import { GroupsModule } from './groups/groups.module';
+import { RatingsModule } from './ratings/ratings.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { StudentsModule } from './students/students.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TelegramsModule } from './telegrams/telegrams.module';
 
 @Module({
   imports: [
@@ -12,6 +21,15 @@ import { join } from 'path';
     }),
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    AdminsModule,
+    AssistantsModule,
+    DirectionsModule,
+    GroupsModule,
+    RatingsModule,
+    RoomsModule,
+    StudentsModule,
+    TasksModule,
+    TelegramsModule,
   ],
   controllers: [],
   providers: [],
